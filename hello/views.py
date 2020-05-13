4060fab838df0e97504008ce5881c936a8dd8a63
mohammadfazil-shaik-sf <mohammadfazil.shaik@heroku.com>
2020-05-13 10:59:05 +0530

    final

--- hello/views.py
+++ hello/views.py
@@ -5,8 +5,8 @@ from .models import Greeting
 import requests
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
-    # return render(request, "index.html")
+    # return HttpResponse('Hello from Python!')
+     return render(request, "index.html")
     # r = requests.get('http://httpbin.org/status/418')
     # print(r.text)
     # return HttpResponse('<pre>' + r.text + '</pre>')
